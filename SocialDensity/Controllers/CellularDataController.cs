bdf596651561c3095ee3e7863785ff21a50cfd6f
ceabdullah <ceabdullahdemirtas@gmail.com>
2015-04-15 08:54:42 +0300

    first release

---
+++ SocialDensity/Controllers/CellularDataController.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialDensity.Controllers
+{
+    public class CellularDataController : Controller
+    {
+
+        public static string opencellidBaseUrl
+        {
+            get { return ConfigurationManager.AppSettings["opencellidBaseUrl"]; }
+        }
+
+        public static string opencellidApiKey
+        {
+            get { return ConfigurationManager.AppSettings["opencellidApiKey"]; }
+        }
+
+        // GET: CellularData
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCellsByCoordinate(string minLat, string minLon, string maxLat, string maxLon)
+        {
+            using (var wb = new WebClient())
+            {
+                try
+                {
+                    string response = wb.DownloadString(opencellidBaseUrl + "cell/getInArea" + "?" + "key=" + opencellidApiKey + "&BBOX=" + minLat + "," + minLon + "," + maxLat + "," + maxLon + "&format=json");
+                    return Json(response);
+                }
+                catch (Exception e)
+                {
+                    return Json("failure");
+                }
+            }
+        }
+
+    }
+}
---
+++ SocialDensity/Controllers/MapController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialDensity.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Google()
+        {
+            return View();
+        }
+
+    }
+}
